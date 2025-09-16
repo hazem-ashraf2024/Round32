79c0f49d970b9266a795256c43b6d73391b82715
hazem-ashraf2024 <ashrafhazem292@gmail.com>
2025-09-16 13:53:48 +0300

    init commit

---
+++ src/test/java/testpackage/TestClass.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+    @Test
+    public void sampleTest() {
+        System.out.println("This is a sample test.");
+    }
+}
